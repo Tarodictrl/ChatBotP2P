8f88578d895b7c81043967f27e8c911e24507edd
Tarodictrl <vudi600@gmail.com>
2022-11-06 17:48:08 +0700

    Initial commit

--- binanceparser.py
+++ binanceparser.py
@@ -1,146 +1,102 @@
-import json
-
-from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+from binance import Client
 from config import *
 
 
-class Trade:
-
-    def __init__(self, start_base: str, start_quote: str, start_count: int):
+class TriangularArbitration:
+    def __init__(self, start_quote: str, start_count: int = 1000):
         self._client = Client(API_KEY, SECRET_KEY)
-        self._start_base = start_base
         self._start_quote = start_quote
-        self._start_symbol = self._start_base + self._start_quote
-        self._last_symbol = self._start_symbol
-        self._last_trade = {}
-        self._trades = []
-        self._circles = []
+        self._comission = 0.1
         self._start_count = start_count
-        self._count = self._start_count
-        self._base = self._start_base
-        self._quote = self._start_quote
-        self._data = self.get_data()
-        self._bundles = self.find_bundles()
+        self._symbols = self.get_symbols()
+        self._connexions = self.get_connexions(self._start_quote)
+        self._all_connexions = [con["symbol"] for con in self._symbols]
+        self._prices = self.get_prices()
 
-    @staticmethod
-    def get_bids(death):
-        return death["bids"]
+    def base_to_quote(self, base_value, bid) -> float:
+        base_value = float(base_value)
+        bid = float(bid)
+        full_comission = base_value / 100 * self._comission
+        return bid * (base_value - full_comission)
 
-    @staticmethod
-    def get_asks(death):
-        return death["asks"]
+    def quote_to_base(self, quote_value, ask) -> float:
+        quote_value = float(quote_value)
+        ask = float(ask)
+        full_comission = quote_value / 100 * self._comission
+        return (quote_value - full_comission) / ask
 
-    @staticmethod
-    def get_data():
-        with open("symbols.json") as f: return json.load(f)["data"]
+    def get_symbols(self) -> list:
+        return self._client.get_exchange_info()["symbols"]
 
-    def get_death(self, symbol="BTCUSDT"):
-        try:
-            return self._client.get_order_book(symbol=symbol)
-        except Exception as e:
-            print(e, symbol)
-            return None
+    def get_prices(self) -> dict:
+        return self._client.get_orderbook_tickers()
 
-    def buy(self):
-        trade = {
-            "base": self._base,
-            "quote": self._quote,
-            "before_count": self._count,
-            "after_count": 0,
-            "ask": 0,
-        }
-        self._last_symbol = self._quote + self._base
-        death = self.get_death(self._last_symbol)
-        asks = self.get_asks(death)
-        self._count = self._count / float(asks[0][0])
-        trade["after_count"] = self._count
-        trade["ask"] = asks[0][0]
-        self._base = self._quote
-        self._last_trade = trade
-        self._trades.append(trade)
-
-    def sell(self, quote):
-        self._quote = quote
-        trade = {
-            "base": self._base,
-            "quote": self._quote,
-            "before_count": self._count,
-            "after_count": 0,
-            "bid": 0,
-        }
-        self._last_symbol = self._quote + self._base
-        death = self.get_death(self._last_symbol)
-        bids = self.get_bids(death)
-        self._count = self._count / float(bids[0][0])
-        trade["after_count"] = self._count
-        trade["ask"] = bids[0][0]
-        self._last_trade = trade
-        self._trades.append(trade)
-        self._base = self._quote
-
-    def end_circle(self):
-        trade = {
-            "base": self._base,
-            "quote": self._start_base,
-            "before_count": self._count,
-            "after_count": 0,
-            "bid": 0,
-        }
-        self._last_symbol = self._base + self._start_base
-        death = self.get_death(self._last_symbol)
-        bids = self.get_bids(death)
-        self._count = self._count * float(bids[0][0])
-        trade["after_count"] = self._count
-        trade["ask"] = bids[0][0]
-        self._last_trade = trade
-        self._trades.append(trade)
-        circle = {}
-        for i, t in enumerate(self._trades, start=1):
-            circle[f"{i}"] = str(t["quote"] + t["base"])
-        circle["spred"] = self.get_spred()
-        circle["spred_p"] = self.get_spred_percent()
-        self._circles.append(circle)
-
-    def get_spred(self):
-        return self._count - self._start_count
-
-    def get_spred_percent(self):
-        percentage = 100 * float(self._count - self._start_count) / float(self._start_count)
-        return percentage
-
-    def get_last_trade(self):
-        return self._last_trade
-
-    def get_all_trades(self):
-        return self._trades
-
-    def find_bundles(self) -> list:
+    def get_connexions(self, quote) -> list:
         array = []
-        for d in self._data:
-            if d["quote"] == self._quote:
-                array.append([d["base"], d["quote"]])
+        for symbol in self._symbols:
+            if symbol["quoteAsset"] == quote:
+                array.append(symbol["baseAsset"] + "/" + symbol["quoteAsset"])
         return array
 
-    def get_bundles(self):
-        return self._bundles
-
-    def reset(self):
-        self._start_symbol = self._start_base + self._start_quote
-        self._last_symbol = self._start_symbol
-        self._count = self._start_count
-        self._base = self._start_base
-        self._quote = self._start_quote
-        self._trades = []
+    def find_orderbook_ticker(self, symbols: list):
+        array = []
+        for price in self._prices:
+            if price["symbol"] in symbols:
+                array.append(price)
+        return array
 
     def get_circles(self):
-        return self._circles
+        circles = []
+        for con in self._connexions:
+            base, quote = con.split("/")
+            for con2 in self.get_connexions(base):
+                base2, quote2 = con2.split("/")
+                if base2 + self._start_quote not in self._all_connexions: continue
+                symbols = [base + quote, base2 + quote2, base2 + self._start_quote]
+                circle = self.find_orderbook_ticker(symbols)
+                circles.append(circle)
+        return circles
 
+    def normalize_circle(self, circle):
+        array = [0.0] * 3
+        for symbol in circle:
+            bid = float(symbol["bidPrice"])
+            ask = float(symbol["askPrice"])
+            if bid == 0 or ask == 0: return None
+            if symbol["symbol"].endswith(self._start_quote):
+                if array[0] == 0:
+                    array[0] = symbol
+                else:
+                    array[2] = symbol
+            else:
+                array[1] = symbol
 
-if __name__ == '__main__':
-    trade = Trade("USDT", "BTC", 1000)
-    trade.buy()
-    trade.sell("XRP")
-    trade.end_circle()
-    print(trade.get_all_trades())
-    print(trade.get_spred())
-    print(trade.get_spred_percent())
+        return array
+
+    def find_spreads(self):
+        self._prices = self.get_prices()
+        trades = []
+        circles = self.get_circles()
+        for circle in circles:
+            c = self.normalize_circle(circle)
+            count = self._start_count
+            base1 = ""
+            if c is None: continue
+            if len(c) == 3:
+                for i, symbol in enumerate(c):
+                    bid = float(symbol["bidPrice"])
+                    ask = float(symbol["askPrice"])
+                    if i == 0:
+                        base1 = symbol["symbol"].replace(self._start_quote, "")
+                        count = self.quote_to_base(count, ask)
+                    elif i == 1:
+                        if symbol["symbol"].startswith(base1):
+                            count = self.base_to_quote(count, bid)
+                        else:
+                            count = self.quote_to_base(count, ask)
+                    elif i == 2:
+                        count = self.base_to_quote(count, bid)
+            spred = 100 * float(count - self._start_count) / float(self._start_count)
+            c.append(spred)
+            trades.append(c)
+        return trades
--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
-import threading
+import json
+import time
 from config import *
 import telebot
-from telebot import types
-import json
 from binanceparser import *
-import time
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -12,7 +10,7 @@ with open("messages.json", encoding="utf-8") as f: messages = json.load(f)
 
 
 def circles_sort(circle: list) -> list:
-    return sorted(circle, key=lambda d: d['spred'], reverse=True)
+    return sorted(circle, key=lambda d: d[-1], reverse=True)
 
 
 @bot.message_handler(commands=['start'])
@@ -29,22 +27,19 @@ def send_help(message):
 def search_spreads(message):
     start_time = time.time()
     bot.send_message(message.chat.id, messages["search_spreads_message"])
-    trade = Trade("USDT", "BTC", 1000)
-    bundles = trade.get_bundles()
-    for bundle in bundles[:10]:
-        base = bundle[0]
-        trade.buy()
-        trade.sell(base)
-        trade.end_circle()
-        trade.reset()
-
-    circles = circles_sort(trade.get_circles())[:5]
-    for t in circles:
-        circle = f'{t["1"]} -> {t["2"]} -> {t["3"]}'
-        spred = t["spred"]
-        spred_p = t["spred_p"]
-        bot.send_message(message.chat.id, text=messages["spred_message"].format(spred, spred_p, circle), parse_mode="Markdown")
-    bot.delete_message(message.chat.id, message.id+1)
+    trade = TriangularArbitration("USDT")
+    circles = trade.find_spreads()
+    circles = circles_sort(circles)
+    for circle in circles[:5]:
+        bot.send_message(message.chat.id,
+                         "*Спред: {0}\n{1} - {2}\n{3} - {4}\n{5} - {6}*".format(circle[-1], circle[0]["symbol"],
+                                                                                circle[0]["askPrice"],
+                                                                                circle[1]["symbol"],
+                                                                                circle[1]["askPrice"],
+                                                                                (circle[2])["symbol"],
+                                                                                circle[0]["bidPrice"]),
+                         parse_mode="Markdown")
+    bot.delete_message(message.chat.id, message.id + 1)
     bot.send_message(message.chat.id, "--- %s seconds ---" % (time.time() - start_time))
 
 
